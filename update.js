97718eef16c47723fcb9b60a412793d43bb7f51f
henr-y <mail@explodingcamera.com>
2016-05-26 22:39:42 +0200

    Switched npm modules

--- update.js
+++ update.js
@@ -1,21 +1,28 @@
-const ghdownload = require('github-download');
+const gitdl = require('download-git-repo');
 const request = require('request');
-var prompt = require('prompt-sync')();
+const yesno = require('yesno');
+const exec = require('child_process').exec;
 
 function download(tag) {
-  ghdownload({user: 'musiqpad', repo: 'mqp-server', ref: tag}, process.cwd() + "/test") // + "/test" is only for testing and will be removed when this is released!
-  .on('dir', function(dir) {
-    console.log(dir)
-  })
-  .on('file', function(file) {
-    console.log(file)
-  })
-  .on('error', function(err) {
-    console.error(err)
-  })
-  .on('end', function() {
-    console.log('end');
-  })
+  gitdl('musiqpad/mqp-server#' + tag, process.cwd() + "/test", function (err) {
+    if (err) {
+      console.log(colors.red('Error: '.error) + err);
+      process.exit();
+    } else {
+      console.log('Download finished, now (re)installing dependencies');
+      console.log('This step might take some time ...');
+      exec('npm install', { cwd: (process.cwd()) + "/test"}, function (error, stdout, stderr) {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        else {
+          console.log('Succesfully installed all dependencies of musiqpad');
+          process.exit();
+        }
+      });
+    }
+  });
 }
 
 module.exports = function update() {
@@ -27,13 +34,16 @@ module.exports = function update() {
   };
   request(options, function (err, res, data) {
     if (!err && res.statusCode == 200) {
-      var q = prompt('This will overide all existing code exept new files and modules. Do you want to continue? (y/N) ').toLowerCase();
-      if(q == "y") {
-        console.log("Now updating to latest version (" + JSON.parse(data).tag_name + ")!");
-        download(JSON.parse(data).tag_name)
-      }
-      else
-        process.exit();
+      yesno.ask('This will overide all existing code exept new files and modules. Do you want to continue? (y/N)', false, function (ok) {
+        if(ok) {
+          console.log("Now updating to latest version (" + JSON.parse(data).tag_name + ")!");
+          download(JSON.parse(data).tag_name)
+        }
+        else
+          process.exit();
+      });
     }
+    else
+      console.log('Error getting the latest musiqpad version: ' + err)
   })
 }
