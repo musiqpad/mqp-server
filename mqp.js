7f0a738ace8a173c7822651a6495192b67a0224d
henr-y <mail@explodingcamera.com>
2016-05-29 19:58:47 +0200

    Made login better + stuff

--- mqp.js
+++ mqp.js
@@ -7,11 +7,12 @@ const updateNotifier = require('update-notifier');
 const pkg = require('./package.json');
 const boxen = require('boxen');
 const update = require('./update');
+const tail = require('file-tail');
 
 function getRunningPid(callback) {
   fs.readFile(__dirname + '/pidfile', {
-    encoding: 'utf-8'
-  }, function(err, pid) {
+    encoding: 'utf-8',
+  }, function (err, pid) {
     if (err) {
       return callback(err);
     }
@@ -19,7 +20,7 @@ function getRunningPid(callback) {
     try {
       process.kill(parseInt(pid, 10), 0);
       callback(null, parseInt(pid, 10));
-    } catch(e) {
+    } catch (e) {
       callback(e);
     }
   });
@@ -28,72 +29,77 @@ function getRunningPid(callback) {
 function checkForUpdates() {
   var notifier = updateNotifier({
     pkg,
-    updateCheckInterval: 0
+    updateCheckInterval: 0,
   });
-  if(notifier.update) {
+  if (notifier.update) {
     var message = '\n' + boxen('Update available ' + chalk.dim(notifier.update.current) + chalk.reset(' → ') + chalk.green(notifier.update.latest), {
       padding: 1,
       margin: 1,
       borderColor: 'yellow',
-      borderStyle: 'round'
+      borderStyle: 'round',
     });
     console.log(message);
   }
 }
 
-switch(process.argv[2]) {
+switch (process.argv[2]) {
   case 'start':
     console.log('\nStarting musiqpad');
-    console.log('  "' + chalk.yellow.bold('node mqp.js stop') + '" to stop the musiqpad server');
-    console.log('  "' + chalk.yellow.bold('node mqp.js log') + '" to view server output');
-    console.log('  "' + chalk.yellow.bold('node mqp.js restart') + '" to restart musiqpad');
+    console.log('  "' + chalk.yellow.bold('npm stop') + '" to stop the musiqpad server');
+    console.log('  "' + chalk.yellow.bold('npm run log') + '" to view server output');
+    console.log('  "' + chalk.yellow.bold('npm restart') + '" to restart musiqpad');
 
     // Spawn a new musiqpad daemon process, might need some more settings but I'm waiting for the new config storage for that.
-    daemon.daemon(__dirname + "/app.js", "--daemon", {
+    daemon.daemon(__dirname + '/app.js', '--daemon', {
       stdout: fs.openSync(path.join(process.cwd(), 'log.txt'), 'a'),
     });
     break;
   case 'stop':
-		getRunningPid(function(err, pid) {
-			if (!err) {
-				process.kill(pid, 'SIGTERM');
-				console.log('Stopping musiqpad!')
-			} else {
-				console.log('Musiqpad is already stopped.');
-			}
-		});
-		break;
-  case 'restart':
-		getRunningPid(function(err, pid) {
-			if (!err) {
-				process.kill(pid, 'SIGHUP');
-				console.log('\nRestarting musiqpad'.bold);
-			} else {
-				console.log('musiqpad could not be restarted, as a running instance could not be found.');
-			}
-		});
-		break;
-  case 'log':
-		console.log('Type ' + 'Ctrl-C ' + 'to exit');
-		cproc.spawn('tail', ['-F', './log.txt'], {
-			cwd: __dirname,
-			stdio: 'inherit'
-		});
-    
-    // TODO: use tail module for windows support
-		break;
-  case 'update':
-    getRunningPid(function(err, pid) {
+    getRunningPid(function (err, pid) {
       if (!err) {
         process.kill(pid, 'SIGTERM');
-        console.log('Stopping musiqpad!')
+        console.log('Stopping musiqpad!');
+      } else {
+        console.log('Musiqpad is already stopped.');
       }
+    });
+
+    break;
+  case 'restart':
+    getRunningPid(function (err, pid) {
+      if (!err) {
+        process.kill(pid, 'SIGHUP');
+        console.log('\nRestarting musiqpad'.bold);
+      } else {
+        console.log('musiqpad could not be restarted, as a running instance could not be found.');
+      }
+    });
+
+    break;
+  case 'log':
+    console.log('Type ' + 'Ctrl-C ' + 'to exit');
+
+    ft = tail.startTailing('./log.txt');
+    ft.on('line', function (line) {
+      console.log(line);
+    });
+
+    break;
+
+  case 'update':
+    getRunningPid(function (err, pid) {
+      if (!err) {
+        process.kill(pid, 'SIGTERM');
+        console.log('Stopping musiqpad!');
+      }
+
       update();
     });
-		break;
+
+    break;
   default:
     console.log('Welcome to musiqpad!');
-    console.log('Usage: node mqp.js {start|stop|restart|log|update}');
+    console.log('Usage: npm run {start|stop|restart|log|update}');
     // TODO: Add infos for each cmd
     break;
 }
--- update.js
+++ update.js
@@ -4,19 +4,18 @@ const yesno = require('yesno');
 const exec = require('child_process').exec;
 
 function download(tag) {
-  gitdl('musiqpad/mqp-server#' + tag, process.cwd() + "/test", function (err) {
+  gitdl('musiqpad/mqp-server#' + tag, process.cwd() + '/test', function (err) {
     if (err) {
       console.log(colors.red('Error: '.error) + err);
       process.exit();
     } else {
       console.log('Download finished, now (re)installing dependencies');
       console.log('This step might take some time ...');
-      exec('npm install', { cwd: (process.cwd()) + "/test"}, function (error, stdout, stderr) {
+      exec('npm install', { cwd: (process.cwd()) + '/test' }, function (error, stdout, stderr) {
         if (error) {
           console.error(`exec error: ${error}`);
           return;
-        }
-        else {
+        } else {
           console.log('Succesfully installed all dependencies of musiqpad');
           process.exit();
         }
@@ -27,23 +26,21 @@ function download(tag) {
 
 module.exports = function update() {
   var options = {
-  url: 'https://api.github.com/repos/musiqpad/mqp-server/releases/latest',
+    url: 'https://api.github.com/repos/musiqpad/mqp-server/releases/latest',
     headers: {
-      'User-Agent': 'request'
-    }
+      'User-Agent': 'request',
+    },
   };
   request(options, function (err, res, data) {
     if (!err && res.statusCode == 200) {
       yesno.ask('This will overide all existing code exept new files and modules. Do you want to continue? (y/N)', false, function (ok) {
-        if(ok) {
-          console.log("Now updating to latest version (" + JSON.parse(data).tag_name + ")!");
-          download(JSON.parse(data).tag_name)
-        }
-        else
+        if (ok) {
+          console.log('Now updating to latest version (' + JSON.parse(data).tag_name + ')!');
+          download(JSON.parse(data).tag_name);
+        } else
           process.exit();
       });
-    }
-    else
-      console.log('Error getting the latest musiqpad version: ' + err)
-  })
-}
+    } else
+      console.log('Error getting the latest musiqpad version: ' + err);
+  });
+};
