92e7b88009b96aed6bbe59cbf879fc0958959f00
Henry Gressmann <mail@explodingcamera.com>
2016-07-08 21:57:29 +0200

    First working version with nconf

--- mqp.js
+++ mqp.js
@@ -1,5 +1,4 @@
 const chalk = require('chalk');
-const cproc = require('child_process');
 const fs = require('fs');
 const daemon = require('daemon');
 const path = require('path');
@@ -13,14 +12,13 @@ const notifier = updateNotifier({
   updateCheckInterval: 0,
 });
 if (notifier.update) {
-  console.log('Update available ' + chalk.dim(notifier.update.current) + chalk.reset(' → ') + chalk.green(notifier.update.latest));
-} else {
+  console.log(`Update available ${chalk.dim(notifier.update.current)}${chalk.reset(' → ')}${chalk.green(notifier.update.latest)}`);
 }
 
 function getRunningPid(callback) {
-  fs.readFile(__dirname + '/pidfile', {
+  fs.readFile(`${__dirname}/pidfile`, {
     encoding: 'utf-8',
-  }, function (err, pid) {
+  }, (err, pid) => {
     if (err) {
       return callback(err);
     }
@@ -36,17 +34,17 @@ function getRunningPid(callback) {
 
 switch (process.argv[2]) {
   case 'start':
-    getRunningPid(function (err, pid) {
+    getRunningPid((err, pid) => {
       if (!err) {
         console.log('Musiqpad is already running!');
       } else {
         console.log('\nStarting musiqpad');
-        console.log('  "' + chalk.yellow.bold('npm stop') + '" to stop the musiqpad server');
-        console.log('  "' + chalk.yellow.bold('npm run log') + '" to view server output');
-        console.log('  "' + chalk.yellow.bold('npm restart') + '" to restart musiqpad');
+        console.log(`  "${chalk.yellow.bold('npm stop')}" to stop the musiqpad server`);
+        console.log(`  "${chalk.yellow.bold('npm run log')}" to view server output`);
+        console.log(`  "${chalk.yellow.bold('npm restart')}" to restart musiqpad`);
 
         // Spawn a new musiqpad daemon process, might need some more settings but I'm waiting for the new config storage for that.
-        daemon.daemon(__dirname + '/start.js', '--daemon', {
+        daemon.daemon(`${__dirname}/start.js`, '--daemon', {
           stdout: fs.openSync(path.join(process.cwd(), 'log.txt'), 'a'),
         });
       }
@@ -54,7 +52,7 @@ switch (process.argv[2]) {
 
     break;
   case 'stop':
-    getRunningPid(function (err, pid) {
+    getRunningPid((err, pid) => {
       if (!err) {
         process.kill(pid, 'SIGTERM');
         console.log('Stopping musiqpad!');
@@ -65,11 +63,11 @@ switch (process.argv[2]) {
 
     break;
   case 'restart':
-    getRunningPid(function (err, pid) {
+    getRunningPid((err, pid) => {
       if (!err) {
         process.kill(pid, 'SIGTERM');
         console.log('\nRestarting musiqpad');
-        daemon.daemon(__dirname + '/start.js', '--daemon', {
+        daemon.daemon(`${__dirname}/start.js`, '--daemon', {
           stdout: fs.openSync(path.join(process.cwd(), 'log.txt'), 'a'),
         });
 
@@ -79,18 +77,16 @@ switch (process.argv[2]) {
     });
 
     break;
-  case 'log':
-    console.log('Type ' + 'Ctrl-C ' + 'to exit');
-
-    ft = tail.startTailing('./log.txt');
-    ft.on('line', function (line) {
+  case 'log': {
+    console.log('Type Ctrl-C to exit');
+    const ft = tail.startTailing('./log.txt');
+    ft.on('line', line => {
       console.log(line);
     });
-
     break;
-
+  }
   case 'update':
-    getRunningPid(function (err, pid) {
+    getRunningPid((err, pid) => {
       if (!err) {
         process.kill(pid, 'SIGTERM');
         console.log('Stopping musiqpad!');
